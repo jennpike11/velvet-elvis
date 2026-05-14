fbd558fd0ca77d3bbc2ccc3896319024d193c8ec
Jennifer Pike <jpike@csis.org>
2026-05-13 18:07:23 -0700

    pushed live

--- inc/components/single_block-backup.php
+++ inc/components/single_block-backup.php
@@ -12,8 +12,8 @@
     <div class="ve-scene ve-scene--hula is-active" data-scene="0" data-scene-name="home">
       <div class="ve-scene__bg"></div>
       <div class="ve-scene__mid"></div>
-      <img class="ve-scene__bug" src="https://dev-velet-elvis.pikexdigital.com/wp-content/uploads/2026/05/bug.png" alt="">
-      <img class="ve-scene__float ve-scene__float--hula" src="https://dev-velet-elvis.pikexdigital.com/wp-content/uploads/2026/05/hula_girl.png" alt="">
+      <img class="ve-scene__bug" src="https://www.velvetelvisstudios.com/wp-content/uploads/2026/05/bug.png" alt="">
+      <img class="ve-scene__float ve-scene__float--hula" src="https://www.velvetelvisstudios.com/wp-content/uploads/2026/05/hula_girl.png" alt="">
       <div class="ve-scene__copy">
         <h1>Velvet Elvis Studios</h1>
         <div class="ve-scene__description">
@@ -29,8 +29,8 @@
       <div class="ve-scene__mid"></div>
       <div class="ve-scene__ember ve-scene__ember--one"></div>
       <div class="ve-scene__ember ve-scene__ember--two"></div>
-      <img class="ve-scene__float ve-scene__float--volcano" src="https://dev-velet-elvis.pikexdigital.com/wp-content/uploads/2026/05/volcano.png" alt="">
-      <img class="ve-scene__float ve-scene__float--volcano-flowers" src="https://dev-velet-elvis.pikexdigital.com/wp-content/uploads/2026/05/flowers.png" alt="">
+      <img class="ve-scene__float ve-scene__float--volcano" src="https://www.velvetelvisstudios.com/wp-content/uploads/2026/05/volcano.png" alt="">
+      <img class="ve-scene__float ve-scene__float--volcano-flowers" src="https://www.velvetelvisstudios.com/wp-content/uploads/2026/05/flowers.png" alt="">
       <div class="ve-scene__copy">
         <h1>About us</h1>
         <div class="ve-scene__description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</div>
@@ -41,8 +41,8 @@
     <div class="ve-scene ve-scene--players" data-scene="2" data-scene-name="contact">
       <div class="ve-scene__bg"></div>
       <div class="ve-scene__mid"></div>
-      <img class="ve-scene__float ve-scene__float--palms" src="https://dev-velet-elvis.pikexdigital.com/wp-content/uploads/2026/05/palm_trees_2.png" alt="">
-      <img class="ve-scene__float ve-scene__float--players" src="https://dev-velet-elvis.pikexdigital.com/wp-content/uploads/2026/05/players.png" alt="">
+      <img class="ve-scene__float ve-scene__float--palms" src="https://www.velvetelvisstudios.com/wp-content/uploads/2026/05/palm_trees_2.png" alt="">
+      <img class="ve-scene__float ve-scene__float--players" src="https://www.velvetelvisstudios.com/wp-content/uploads/2026/05/players.png" alt="">
       <div class="ve-scene__copy">
         <h1>Contact Us</h1>
         <div class="ve-scene__description">
